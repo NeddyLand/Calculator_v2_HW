042e271ae3010131909d0ebe1c13f1348c43b501
Neddy <Ned@gmail.com>
2024-04-25 18:48:01 +0300

    Добавьте файлы проекта.

---
+++ Calculator_v2_HW/Calculator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v2_HW
+{
+    class Calculator
+    {
+        private double result = 0;
+        Stack<double> results = new Stack<double>();
+
+        public double Add(double value)
+        {
+            results.Push(result);
+            return result += value;
+        }
+        public double Sub(double value)
+        {
+            results.Push(result);
+            return result -= value;
+        }
+        public double Mul(double value)
+        {
+            results.Push(result);
+            return result *= value;
+        }
+        public double Div(double value)
+        {
+            results.Push(result);
+            return result /= value;
+        }
+        public double Div(double value, Calculator calc)
+        {
+            results.Push(result);
+            return result = value / calc.result;
+        }
+        public double Cancel()
+        {
+            if (results.Count > 0)
+            {
+                return result = results.Pop();
+            }
+            return result;
+        }
+        public static double operator +(Calculator a, int b) => a.Add(b);
+        public static double operator +(int b, Calculator a) => a.Add(b);
+        public static double operator +(Calculator a, double b) => a.Add(b);
+        public static double operator +(double b, Calculator a) => a.Add(b);
+        public static double operator -(Calculator a, int b) => a.Sub(b);
+        public static double operator -(int b, Calculator a) => a.Sub(-b);
+        public static double operator -(Calculator a, double b) => a.Sub(b);
+        public static double operator -(double b, Calculator a) => a.Sub(-b);
+        public static double operator *(Calculator a, int b) => a.Mul(b);
+        public static double operator *(int b, Calculator a) => a.Mul(b);
+        public static double operator *(Calculator a, double b) => a.Mul(b);
+        public static double operator *(double b, Calculator a) => a.Mul(b);
+        public static double operator /(Calculator a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+                return a.Div(b);
+        }
+        public static double operator /(int b, Calculator a)
+        {
+            if (a.result == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+                return a.Div(b, a);
+        }
+        public static double operator /(Calculator a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+                return a.Div(b);
+        }
+        public static double operator /(double b, Calculator a)
+        {
+            if (a.result == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+                return a.Div(b, a);
+        }
+        public override string ToString()
+        {
+            return result.ToString();
+        }
+    }
+}
---
+++ Calculator_v2_HW/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Calculator_v2_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            Console.WriteLine($"{calc} + 7 = {calc + 7}");
+            Console.WriteLine($"7 + {calc} = {7 + calc}");
+            Console.WriteLine($"{calc} - 7 = {calc - 7}");
+            Console.WriteLine($"7 - {calc} = {7 - calc}");
+            Console.WriteLine($"{calc} * 7 = {calc * 7}");
+            Console.WriteLine($"7 * {calc} = {7 * calc}");
+            Console.WriteLine($"{calc} / 7 = {calc / 7}");
+            Console.WriteLine($"98 / {calc} = {98 / calc}");
+            Console.WriteLine();
+            Console.WriteLine($"{calc} + 8.5 = {calc + 8.5}");
+            Console.WriteLine($"8.5 + {calc} = {8.5 + calc}");
+            Console.WriteLine($"{calc} - 8.5 = {calc - 8.5}");
+            Console.WriteLine($"8.5 - {calc} = {8.5 - calc}");
+            Console.WriteLine($"{calc} * 8.5 = {calc * 8.5}");
+            Console.WriteLine($"8.5 * {calc} = {8.5 * calc}");
+            Console.WriteLine($"{calc} / 8.5 = {calc / 8.5}");
+            Console.WriteLine($"8.5 / {calc} = {8.5 / calc}");
+        }
+    }
+}
